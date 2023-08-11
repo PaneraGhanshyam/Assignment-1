bd6fec4832a635f5ffdc4333fa5ae4ac5196bb42
panera ghanshyam <ghanshyampanera80@gmail.com>
2023-08-11 04:22:25 -0700

    first commit

---
+++ Assignment-7/project-1/calculator/src/java/com/calculator/calculate.java
@@ -0,0 +1,39 @@
+
+package com.calculator;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.*;
+import javax.servlet.http.*;
+/** 
+ *
+ * @author ghanshyam
+ */
+public class calculate extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
+        res.setContentType("text/html");
+        PrintWriter out=res.getWriter();
+        
+        out.print("<h1>CALCULATOR</h1>");
+        int n1 =Integer.parseInt(req.getParameter("num1"));
+        out.print("<h1>"+n1); 
+        
+        String op=req.getParameter("operation");
+        out.print(op);
+         
+        int n2=Integer.parseInt(req.getParameter("num2"));
+        out.print(n2+" :");
+        
+        if("+".equals(op))
+            out.print(" "+(n1+n2)+"</h1");
+        else if("-".equals(op))
+            out.print(" "+(n1-n2)+"</h1");
+        else if("*".equals(op))
+            out.print(" "+(n1*n2)+"</h1");
+        else if("/".equals(op))
+            out.print(" "+(n1/n2)+"</h1");
+    }
+    
+}
